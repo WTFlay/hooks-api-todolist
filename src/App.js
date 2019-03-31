e1180c58ba2a837c474189ddeddece054ba0db8b
WTFlay <schrie_f@etna-alternance.net>
2019-03-31 18:45:12 +0200

    Clean app and add css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="todolist not-done">
+              <h1>Todos</h1>
+              <input type="text" className="form-control add-todo" placeholder="Add todo"/>
+                <button id="checkAll" className="btn btn-success">Mark all as done</button>
+                <hr/>
+                  <ul id="sortable" className="list-unstyled">
+                    <li className="ui-state-default">
+                      <div className="checkbox">
+                        <label>
+                          <input type="checkbox" value=""/>Take out the trash</label>
+                      </div>
+                    </li>
+                    <li className="ui-state-default">
+                      <div className="checkbox">
+                        <label>
+                          <input type="checkbox" value=""/>Buy bread</label>
+                      </div>
+                    </li>
+                    <li className="ui-state-default">
+                      <div className="checkbox">
+                        <label>
+                          <input type="checkbox" value=""/>Teach penguins to fly</label>
+                      </div>
+                    </li>
+                  </ul>
+                  <div className="todo-footer">
+                    <strong><span className="count-todos"/></strong> Items Left
+                  </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="todolist">
+              <h1>Already Done</h1>
+              <ul id="done-items" className="list-unstyled">
+                <li>
+                  Some item
+                  <button className="remove-item btn btn-default btn-xs pull-right">
+                    <span className="glyphicon glyphicon-remove"/>
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-    );
+  );
   }
 }
 
